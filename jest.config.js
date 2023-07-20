3538b0cb0ba11157a9e65a40182e39f54eec8bd6
Miguel Augusto Caligares <mcaligares@gmail.com>
2023-07-19 22:09:07 -0300

    fix: jest config file

--- jest.config.js
+++ jest.config.js
@@ -6,15 +6,9 @@ module.exports = {
   testRegex: './__tests__/.*\\.(test|spec)?\\.(ts|ts)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   rootDir: '.',
-  roots: ['<rootDir>/src'],
-  moduleNameMapper: {
-    'controllers/(.*)': '<rootDir>/src/controllers/$1',
-    'middlewares/(.*)': '<rootDir>/src/middlewares/$1',
-    'repositories/(.*)': '<rootDir>/src/repositories/$1',
-    'routes/(.*)': '<rootDir>/src/routes/$1',
-    'services/(.*)': '<rootDir>/src/services/$1',
-    'utils/(.*)': '<rootDir>/src/utils/$1'
-  },
+  roots: ['<rootDir>/src', '<rootDir>/__tests__'],
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: ['./**/node_modules/'],
   coverageThreshold: {
     global: {
       branches: 75,
